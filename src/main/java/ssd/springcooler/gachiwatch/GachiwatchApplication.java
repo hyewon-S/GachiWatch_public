2d726d6f4abd6fbf2d2cbaa611cf48ecf52600e0
이혜정 <lhj1227lhj@naver.com>
2025-04-24 14:40:49 +0900

    project commit

---
+++ src/main/java/ssd/springcooler/gachiwatch/GachiwatchApplication.java
@@ -0,0 +1,13 @@
+package ssd.springcooler.gachiwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachiwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachiwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssd/springcooler/gachiwatch/GachiwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ssd.springcooler.gachiwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachiwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
