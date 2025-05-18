f956c0966ffc685259d503ae1b9831af540a1ef2
이혜정 <lhj1227lhj@naver.com>
2025-05-18 10:43:58 +0900

    (feat) MyBatis 클래스 일부 작성 (test)

---
+++ src/main/java/ssd/springcooler/gachiwatch/dao/mybatis/MybatisCrewDao.java
@@ -0,0 +1,66 @@
+package ssd.springcooler.gachiwatch.dao.mybatis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ssd.springcooler.gachiwatch.dao.CrewDao;
+import ssd.springcooler.gachiwatch.dao.mybatis.mapper.CrewMapper;
+import ssd.springcooler.gachiwatch.domain.Crew;
+import ssd.springcooler.gachiwatch.domain.Member;
+import ssd.springcooler.gachiwatch.domain.Platform;
+
+import java.util.List;
+
+
+public class MybatisCrewDao implements CrewDao {
+    @Autowired
+    private CrewMapper crewMapper;
+
+    @Override
+    public Member getCaptain(int crewId) {
+        return crewMapper.getCaptain(crewId);
+    }
+
+    @Override
+    public List<Member> getMembers(int crewId) {
+        return crewMapper.getMemberByCrewId(crewId);
+    }
+
+    @Override
+    public List<Crew> getCrewList() {
+        return crewMapper.getCrewList();
+    }
+
+    @Override
+    public List<Crew> getCrewList(List<Platform> platforms) {
+        return crewMapper.getCrewListByPlatforms(platforms);
+    }
+
+    @Override
+    public Crew getCrew(int crewId) {
+        return crewMapper.getCrewByCrewId(crewId);
+    }
+
+    @Override
+    public Crew insertCrew(Crew crew) {
+        return crewMapper.insertCrew(crew);
+    }
+
+    @Override
+    public Crew updateCrew(Crew crew) {
+        return crewMapper.updateCrew(crew);
+    }
+
+    @Override
+    public boolean deleteCrew(Crew crew) {
+        return crewMapper.deleteCrew(crew);
+    }
+
+    @Override
+    public boolean insertMember(int crewId, Member member) {
+        return crewMapper.insertMember(crewId, member);
+    }
+
+    @Override
+    public boolean deleteMember(int crewId, Member member) {
+        return crewMapper.deleteMember(crewId, member);
+    }
+}
