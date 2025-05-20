d8e98eaf72020c17a629b42b8550a1beeb46e3c0
anstp28 <moonsy9165@gmail.com>
2025-05-20 16:05:34 +0900

    member mapper 코드 추가

---
+++ src/main/java/ssd/springcooler/gachiwatch/dao/mybatis/MyBatisMemberDao.java
@@ -0,0 +1,117 @@
+package ssd.springcooler.gachiwatch.dao.mybatis;
+
+package com.example.demo.dao.mybatis;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.dao.MemberDao;
+import com.example.demo.dto.*;
+import com.example.demo.domain.Member;
+import com.example.demo.mapper.MemberMapper;
+import ssd.springcooler.gachiwatch.dto.MemberRegisterDto;
+
+@Repository
+public class MyBatisMemberDao implements MemberDao {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Override
+    public void insertMember(MemberRegisterDto dto) {
+        memberMapper.insertMember(dto);
+    }
+
+    @Override
+    public void insertMemberGenres(Long memberId, List<Long> genreIds) {
+        memberMapper.insertMemberGenres(memberId, genreIds);
+    }
+
+    @Override
+    public void insertMemberOtts(Long memberId, List<Long> ottIds) {
+        memberMapper.insertMemberOtts(memberId, ottIds);
+    }
+
+    @Override
+    public MemberDto findByEmailAndPassword(LoginDto loginDto) {
+        return memberMapper.findByEmailAndPassword(loginDto);
+    }
+
+    @Override
+    public MemberDto findByKakaoEmail(String email) {
+        return memberMapper.findByKakaoEmail(email);
+    }
+
+    @Override
+    public void updateProfile(Long memberId, ProfileUpdateDto dto) {
+        memberMapper.updateProfile(memberId, dto);
+    }
+
+    @Override
+    public void updatePassword(Long memberId, String newPassword) {
+        memberMapper.updatePassword(memberId, newPassword);
+    }
+
+    @Override
+    public void updateProfileImage(Long memberId, String imageUrl) {
+        memberMapper.updateProfileImage(memberId, imageUrl);
+    }
+
+    @Override
+    public void deleteMemberOtts(Long memberId) {
+        memberMapper.deleteMemberOtts(memberId);
+    }
+
+    @Override
+    public void deleteMemberGenres(Long memberId) {
+        memberMapper.deleteMemberGenres(memberId);
+    }
+
+    @Override
+    public List<ReviewDto> selectMyReviews(Long memberId) {
+        return memberMapper.selectMyReviews(memberId);
+    }
+
+    @Override
+    public void deleteReviewById(Long reviewId) {
+        memberMapper.deleteReviewById(reviewId);
+    }
+
+    @Override
+    public List<ContentDto> selectWatchedContents(Long memberId) {
+        return memberMapper.selectWatchedContents(memberId);
+    }
+
+    @Override
+    public void deleteWatchedContentById(Long contentId) {
+        memberMapper.deleteWatchedContentById(contentId);
+    }
+
+    @Override
+    public List<ReportDto> selectReportsByMe(Long memberId) {
+        return memberMapper.selectReportsByMe(memberId);
+    }
+
+    @Override
+    public List<ReportReceivedDto> selectReportsAgainstMe(Long memberId) {
+        return memberMapper.selectReportsAgainstMe(memberId);
+    }
+
+    @Override
+    public List<CrewDto> selectMyCrews(Long memberId) {
+        return memberMapper.selectMyCrews(memberId);
+    }
+
+    @Override
+    public List<ContentDto> selectLikedContents(Long memberId) {
+        return memberMapper.selectLikedContents(memberId);
+    }
+
+    @Override
+    public void deleteMemberById(Long memberId) {
+        memberMapper.deleteMemberById(memberId);
+    }
+}
+
