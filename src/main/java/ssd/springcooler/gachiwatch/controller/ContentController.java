dd0341a590c0378c905ad706576ebe39f2355b1b
hyewon-S <shw12421@hanmail.net>
2025-06-08 01:41:36 +0900

    Feat: 통합 페이지 회원용 추가, detail 페이지 밑작업

--- src/main/java/ssd/springcooler/gachiwatch/controller/ContentController.java
+++ src/main/java/ssd/springcooler/gachiwatch/controller/ContentController.java
@@ -1,11 +1,13 @@
 package ssd.springcooler.gachiwatch.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ssd.springcooler.gachiwatch.service.ContentService;
 
 @Controller
@@ -15,9 +17,29 @@ public class ContentController {
     private ContentService contentService;
 
     @GetMapping("/search")
-    public String search(Model model) {
+    public String search(Model model, HttpSession session) {
+        Object user = session.getAttribute("user");
+
+        if(user != null) { //로그인 여부 boolean 에 세팅
+            model.addAttribute("isLoggedIn", true);
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
         model.addAttribute("contentList", contentService.getContentSummary());
         return "content/searchPage";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam("contentId") int contentId, Model model, HttpSession session) {
+        Object user = session.getAttribute("user");
+
+        if(user != null) { //로그인 여부 boolean 에 세팅
+            model.addAttribute("isLoggedIn", true);
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
+        model.addAttribute("contentInfo", contentService.getContentDetail(contentId));
+        return "content/detailPage"; //여기서 review 쪽으로 redirect 한번 해줄것
     }
 
     //API 에서 콘텐츠 받아와서 DB에 데이터 넣어둠
