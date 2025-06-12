4655a4e8d04f64650deb41adca78d4e57726c795
hyewon-S <shw12421@hanmail.net>
2025-06-13 02:24:11 +0900

    Feat: detail 페이지 리뷰 밑작업, 좋아요/봤어요 버튼 작업

--- src/main/java/ssd/springcooler/gachiwatch/controller/ContentController.java
+++ src/main/java/ssd/springcooler/gachiwatch/controller/ContentController.java
@@ -2,13 +2,14 @@ package ssd.springcooler.gachiwatch.controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ssd.springcooler.gachiwatch.service.ContentService;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/content")
@@ -33,13 +34,40 @@ public class ContentController {
     public String detail(@RequestParam("contentId") int contentId, Model model, HttpSession session) {
         Object user = session.getAttribute("user");
 
-        if(user != null) { //로그인 여부 boolean 에 세팅
+        if(user != null) { //로그인 했음
+            session.setAttribute("user", user);
             model.addAttribute("isLoggedIn", true);
+            //유저가 좋아요를 눌렀는지, 봤어요를 눌렀는지 확인해줘야 함
+            model.addAttribute("heartUrl", "/image/icon/icon-heart-red.png");
+            model.addAttribute("eyeUrl", "/image/icon/icon-eye.png");
         } else {
             model.addAttribute("isLoggedIn", false);
         }
         model.addAttribute("contentInfo", contentService.getContentDetail(contentId));
         return "content/detailPage"; //여기서 review 쪽으로 redirect 한번 해줄것
+    }
+
+    @PostMapping("/likeUpdate")
+    @ResponseBody
+    public ResponseEntity<?> updateContentLike(@RequestParam int contentId, @RequestParam int memberId,
+                                            @RequestParam boolean isLiked,
+                                            Principal principal) {
+        System.out.println("contentId는 : " + contentId);
+        System.out.println("memberId는 : " + memberId); //이게 계속 0으로 뜨는데.. 왜지
+        System.out.println("isLike는" + isLiked);
+
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("/watchedUpdate")
+    @ResponseBody
+    public ResponseEntity<?> updateContentWatched(@RequestParam int contentId, @RequestParam int memberId,
+                                               @RequestParam boolean isWatched,
+                                               Principal principal) {
+        System.out.println("contentId는 : " + contentId);
+        System.out.println("memberId는 : " + memberId);
+        System.out.println("isLike는 : " + isWatched);
+
+        return ResponseEntity.ok().build();
     }
 
     //API 에서 콘텐츠 받아와서 DB에 데이터 넣어둠
