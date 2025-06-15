667d9270b870939f91c56775dfa63a2b2a49846c
이혜정 <lhj1227lhj@naver.com>
2025-06-15 13:48:37 +0900

    chore: 크루 전체보기 view수정

--- src/main/java/ssd/springcooler/gachiwatch/controller/CrewViewController.java
+++ src/main/java/ssd/springcooler/gachiwatch/controller/CrewViewController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ssd.springcooler.gachiwatch.domain.Crew;
 import ssd.springcooler.gachiwatch.domain.Member;
+import ssd.springcooler.gachiwatch.domain.Platform;
 import ssd.springcooler.gachiwatch.service.CrewServiceImpl;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/crew/view")
@@ -23,6 +26,12 @@ public class CrewViewController {
     @GetMapping
     public ModelAndView viewCrews() {
         ModelAndView mav = new ModelAndView();
+        List<Platform> filteredPlatforms = Arrays.stream(Platform.values())
+                .filter(p -> p != Platform.NULL)
+                .collect(Collectors.toList());
+
+        mav.addObject("platforms", filteredPlatforms);
+
         mav.addObject("parties", List.of(
                 new Crew(new Member(), "이름", 3),
                 new Crew(new Member(), "이름2", 4)
